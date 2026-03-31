84dd795e8ada76f755198aa3c9af9c318c70db9a
Abhishek Thakur <abburajput7876@gmail.com>
2026-04-01 02:04:57 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+require("dotenv").config();
+const connectdb = require("./src/config/database")
+const app = express();
+const http = require("http");
+
+
+// this means i am creating server on the top of express app
+const server = http.createServer(app);
+
+
+
+
+
+const PORT = Number(process.env.PORT_NO);
+connectdb().then(() => {
+    console.log("database connection is successfull!!");
+    // in above line we connect our database  then we make our prepared to listening the requests
+    server.listen(PORT, () => {
+        console.log("Our server is running successfully in 9999");
+    })
+}).catch((err) => {
+    console.error("database cannot be connected" + err.message);
+});
+
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const connectdb = async ()=>{
+    await mongoose.connect(process.env.MONGODB_URL);    
+};
+
+module.exports = connectdb; 
